ae213d5bb57afbcb26f0cffe5e853a51822bb2aa
chavaudrey <chavaudreyxav@hotmail.fr>
2022-05-03 19:47:57 +0200

    change ===

--- database.sql
+++ database.sql
@@ -1,73 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `simple-mvc`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `game`
---
-
-CREATE TABLE `unlock_db`.`game` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(100) NOT NULL,
-  `image_url` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`));
-
-
-
---
--- Structure de la table `item`
---
-
-CREATE TABLE IF NOT EXISTS `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 CREATE TABLE IF NOT EXISTS `card` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(100),
   `description` TEXT,
-  `type` VARCHAR(100),
-);
+  `image_url` VARCHAR(100)
+) ENGINE = InnoDB CHARACTER SET latin1 COLLATE latin1_bin ;
 
 CREATE TABLE IF NOT EXISTS `game` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(200),
-  `description` TEXT
-);
+  `name` VARCHAR(150),
+  `description` TEXT,
+  `instruction` VARCHAR(100),
+  `image_url` VARCHAR(100)
+) ENGINE = InnoDB CHARACTER SET latin1 COLLATE latin1_bin ;
 
 CREATE TABLE IF NOT EXISTS `gamecards` (
   `game_id` INT NOT NULL,
   `card_id` INT NOT NULL,
-  `card_number` INT NOT NULL
-);
+  `card_number` INT NOT NULL,
+  `available_on_begin` BOOLEAN NOT NULL DEFAULT 1
+) ENGINE = InnoDB CHARACTER SET latin1 COLLATE latin1_bin ;
 
 ALTER TABLE `gamecards`
-  ADD CONSTRAINT `fk_game_id`
-  FOREIGN KEY (`game_id`)
-  REFERENCES `game`(`id`)
-  ON DELETE CASCADE;
+    ADD CONSTRAINT `fk_game_id`
+    FOREIGN KEY (`game_id`)
+    REFERENCES `game`(`id`)
+    ON DELETE CASCADE;
 
 ALTER TABLE `gamecards`
   ADD CONSTRAINT `fk_card_id`
@@ -77,33 +34,29 @@ ALTER TABLE `gamecards`
 
 ALTER TABLE `gamecards`
   ADD UNIQUE (`game_id`, `card_id`);
---
--- Contenu de la table `item`
---
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+INSERT INTO `game` (`name`, `description`, `instruction`, `image_url`) VALUES 
+("Sherlock, découverte","Initié vous au jeu UNLOCK et les histoires de Sherlock","A l'aide des différentes combinaisons de cartes, recherche la clé qui te mènera vers la sortie.","assets/images/carnet.png"), ("Sherlock, enquête","Initié vous au jeu UNLOCK et les histoires de Sherlock","A l'aide des différentes combinaisons de cartes, recherche la clé qui te mènera vers la sortie.","assets/images/caleche.png"), ("Dr Watson","Initié vous au jeu UNLOCK et les histoires de Sherlock","A l'aide des différentes combinaisons de cartes, recherche la clé qui te mènera vers la sortie.","assets/images/street.png");
+INSERT INTO `card` (`name`, `description`, `image_url`) VALUES
+("Salon", "Voici la pièce où vous êtes enfermés. Plusieurs éléments sont visibles. Vous pouvez maintenant associer les cartes qui vous mèneront vers la sortie.", ""),
+("Câble", "Voici un câble téléphonique.", "/assets/images/cable.png"),
+("Clé", "C'est une clé, qui permet sûrement d'ouvrir une serrure...", "/assets/images/key.png"),
+("Armoire", "Une armoire fermée à clé.", "/assets/images/armoire.png"),
+("Watson", "Il vous connaît bien, il peut peut-être vous aider.", "/assets/images/watson.png"),
+("Porte", "La porte d'entrée est fermée à clé.", "/assets/images/door.png"),
+("Carnet téléphonique", "Ce carnet contient le numéro de téléphone de Watson.", "/assets/images/carnet.png"),
+("Téléphone", "Appel à un ami.", "/assets/images/phone.png"),
+("Livre", "Ce livre contient une clé.", "/assets/images/keydoor.png"),
+("Fil de Téléphone", "Brancher votre téléphone!", "/assets/images/cable.png"),
+("Fin", "", "");
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `gamecards` (`game_id`, `card_id`, `card_number`, `available_on_begin`) VALUES
+(1, 3, 2, 1),
+(1, 4, 1, 1),
+(1, 5, 11, 1),
+(1, 6, 29, 1),
+(1, 10, 4, 1),
+(1, 7, 3, 0),
+(1, 8, 7, 0),
+(1, 9, 18, 0),
+(1, 11, 47, 0);
